b05a49187ed9eb71219ad447e978672a537d0c36
Matthewpickering2000 <matthewpickering2000@gmail.com>
2025-04-09 18:04:12 -0700

    Intit Upload

---
+++ app.js
@@ -0,0 +1,19 @@
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("Slide");
+    let dots = document.getElementsByClassName("dot");
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].classList.add("active");
+    setTimeout(showSlides, 1500);
+}
+showSlides();
